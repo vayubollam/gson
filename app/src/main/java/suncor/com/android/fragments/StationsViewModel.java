858da3c6fc5a6c7b078b430f879a68318d35529c
Hicham Rafia <hichamrafia@hichams-mbp.casanearshore.ma.ibm.com>
2018-12-17 17:06:34 +0100

    add app bar and map, show map based user current location, show alert when gps is off

---
+++ app/src/main/java/suncor/com/android/fragments/StationsViewModel.java
@@ -0,0 +1,8 @@
+package suncor.com.android.fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class StationsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+
+}
--- app/src/main/java/suncor/com/android/ui/SafeDriveActivity.java
+++ app/src/main/java/suncor/com/android/ui/SafeDriveActivity.java
@@ -1,15 +1,34 @@
 package suncor.com.android.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 import suncor.com.android.R;
 
-import android.os.Bundle;
-
-public class SafeDriveActivity extends AppCompatActivity {
+public class SafeDriveActivity extends AppCompatActivity implements View.OnClickListener {
+    private MaterialButton btn_ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safe_drive);
+        btn_ok=findViewById(R.id.btn_drive_safely_ok);
+        btn_ok.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btn_ok)
+            openHomeActivity();
+    }
+
+    private void openHomeActivity() {
+        Intent homeIntent=new Intent(this,HomeActivity.class);
+        startActivity(homeIntent);
+        finish();
     }
 }
