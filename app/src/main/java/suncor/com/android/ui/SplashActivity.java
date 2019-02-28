bdef33651049b5d105ba5dfaaca97541be0c3f39
Hicham Boushaba <hicham.boushaba@ibm.com>
2019-02-28 17:44:46 +0000

    Merged in bugfix/RMP-741 (pull request #50)
    show status bar on Splash Screen

--- app/src/main/java/suncor/com/android/ui/SplashActivity.java
+++ app/src/main/java/suncor/com/android/ui/SplashActivity.java
@@ -7,6 +7,7 @@ import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -16,7 +17,6 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
@@ -28,19 +28,22 @@ import suncor.com.android.mfp.SessionManager;
 import suncor.com.android.ui.home.HomeActivity;
 
 public class SplashActivity extends AppCompatActivity implements Animation.AnimationListener {
-    private RelativeLayout SafetyMessageLayout;
-    Handler safetyMessageHandler = new Handler();
+    Handler delayHandler = new Handler();
+    private final static int ENTER_ANIMATION_DURATION = 1400;
+    private final static int EXIT_ANIMATION_DURATION = 900;
 
     private AppCompatImageView imageRetail;
     private AppCompatImageView backgroundImage;
     private LinearLayout textLayout;
-    private int enterAnimationDuration = 1400;
-    private int exitAnimationDuration = 900;
     private int delayExit = 900;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView()
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
         if (SessionManager.getInstance().isUserLoggedIn()) {
             SessionManager.getInstance().checkLoginState();
         }
@@ -85,9 +88,9 @@ public class SplashActivity extends AppCompatActivity implements Animation.Anima
         animFromBottom.setInterpolator(animInterpolator);
         animFromLet.setInterpolator(animInterpolator);
         animZoomOut.setInterpolator(animInterpolator);
-        animFromBottom.setDuration(enterAnimationDuration);
-        animFromLet.setDuration(enterAnimationDuration);
-        animZoomOut.setDuration(enterAnimationDuration);
+        animFromBottom.setDuration(ENTER_ANIMATION_DURATION);
+        animFromLet.setDuration(ENTER_ANIMATION_DURATION);
+        animZoomOut.setDuration(ENTER_ANIMATION_DURATION);
         animZoomOut.setAnimationListener(this);
 
         backgroundImage.startAnimation(animZoomOut);
@@ -102,7 +105,7 @@ public class SplashActivity extends AppCompatActivity implements Animation.Anima
 
     @Override
     public void onAnimationEnd(Animation animation) {
-        safetyMessageHandler.postDelayed(() -> {
+        delayHandler.postDelayed(() -> {
             startExitAnimation();
         }, delayExit);
     }
@@ -116,7 +119,7 @@ public class SplashActivity extends AppCompatActivity implements Animation.Anima
         ObjectAnimator toBottomAnim = ObjectAnimator.ofFloat(imageRetail, "translationY", screenHeightDiff);
 
         AnimatorSet animSetXY = new AnimatorSet();
-        animSetXY.setDuration(exitAnimationDuration);
+        animSetXY.setDuration(EXIT_ANIMATION_DURATION);
         animSetXY.playTogether(toLeftAnim, toBottomAnim);
         animSetXY.addListener(new AnimatorListenerAdapter() {
             @Override
@@ -160,8 +163,7 @@ public class SplashActivity extends AppCompatActivity implements Animation.Anima
     }
 
     private void openHomeActivity() {
-
-        safetyMessageHandler.removeCallbacksAndMessages(null);
+        delayHandler.removeCallbacksAndMessages(null);
         Intent homeIntent = new Intent(this, HomeActivity.class);
         startActivity(homeIntent);
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
@@ -171,13 +173,13 @@ public class SplashActivity extends AppCompatActivity implements Animation.Anima
     @Override
     protected void onPause() {
         super.onPause();
-        safetyMessageHandler.removeCallbacksAndMessages(null);
+        delayHandler.removeCallbacksAndMessages(null);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        safetyMessageHandler.removeCallbacksAndMessages(null);
+        delayHandler.removeCallbacksAndMessages(null);
     }
 
     public static int getScreenWidth() {
