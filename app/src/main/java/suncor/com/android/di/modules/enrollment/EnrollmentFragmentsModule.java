3888c42f827dfe4068b2638b2e839a0b85bae8b9
Hicham.Rafia <hicham.rafia@ibm.com>
2019-04-04 10:15:17 +0000

    Merged in feature/RMP-1247 (pull request #93)
    Feature/RMP-1247  loading security question

--- app/src/main/java/suncor/com/android/di/modules/enrollment/EnrollmentFragmentsModule.java
+++ app/src/main/java/suncor/com/android/di/modules/enrollment/EnrollmentFragmentsModule.java
@@ -2,14 +2,14 @@ package suncor.com.android.di.modules.enrollment;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
-import suncor.com.android.ui.enrollment.CardQuestion;
+import suncor.com.android.ui.enrollment.CardQuestionFragment;
 import suncor.com.android.ui.enrollment.form.EnrollmentFormFragment;
 import suncor.com.android.ui.enrollment.form.SecurityQuestionFragment;
 
 @Module
 abstract class EnrollmentFragmentsModule {
     @ContributesAndroidInjector
-    abstract CardQuestion contributeCardQuestion();
+    abstract CardQuestionFragment contributeCardQuestion();
 
     @ContributesAndroidInjector
     abstract EnrollmentFormFragment contributeEnrollmentFormFragment();
