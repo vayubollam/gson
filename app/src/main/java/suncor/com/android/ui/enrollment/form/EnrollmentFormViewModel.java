d34b9415676a8ff08ba2f1d4136e5f506e49e07a
hicham rafia <hicham.rafia@ibm.com>
2019-04-12 17:38:14 +0100

    fix alert bug

--- app/src/main/java/suncor/com/android/ui/enrollment/form/EnrollmentFormViewModel.java
+++ app/src/main/java/suncor/com/android/ui/enrollment/form/EnrollmentFormViewModel.java
@@ -342,16 +342,21 @@ public class EnrollmentFormViewModel extends ViewModel {
             return;
         this.cardStatus = cardStatus;
         if (cardStatus.getCardType() == NewEnrollment.EnrollmentType.EXISTING) {
-            firstNameField.setText(cardStatus.getUserInfo().getFirstName());
-            lastNameField.setText(cardStatus.getUserInfo().getLastName());
-            emailInputField.setText(cardStatus.getUserInfo().getEmail());
-            streetAddressField.setText(cardStatus.getAddress().getStreetAddress());
-            cityField.setText(cardStatus.getAddress().getCity());
-            provinceField.setText(cardStatus.getAddress().getProvince());
-            postalCodeField.setText(cardStatus.getAddress().getPostalCode());
-            if (!cardStatus.getAddress().getPhone().isEmpty()) {
-                phoneField.setText(cardStatus.getAddress().getPhone());
+            try {
+                firstNameField.setText(cardStatus.getUserInfo().getFirstName());
+                lastNameField.setText(cardStatus.getUserInfo().getLastName());
+                emailInputField.setText(cardStatus.getUserInfo().getEmail());
+                streetAddressField.setText(cardStatus.getAddress().getStreetAddress());
+                cityField.setText(cardStatus.getAddress().getCity());
+                provinceField.setText(cardStatus.getAddress().getProvince());
+                postalCodeField.setText(cardStatus.getAddress().getPostalCode());
+                if (!cardStatus.getAddress().getPhone().isEmpty()) {
+                    phoneField.setText(cardStatus.getAddress().getPhone());
+                }
+            } catch (Exception ex) {
+
             }
+
         } else {
             lastNameField.setText(cardStatus.getUserInfo().getLastName());
             postalCodeField.setText(cardStatus.getAddress().getPostalCode());
