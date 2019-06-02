66f5caa729f97d05ec81fe3ef53567fba09c2cd8
hicham boushaba <hicham.boushaba@outlook.com>
2019-06-02 18:19:04 +0000

    Refactor HomeFragment to allow swapping layout depending on sign in status

--- app/src/main/java/suncor/com/android/ui/main/home/HomeFragment.java
+++ app/src/main/java/suncor/com/android/ui/main/home/HomeFragment.java
@@ -18,24 +18,20 @@ import javax.inject.Inject;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import suncor.com.android.LocationLiveData;
 import suncor.com.android.R;
-import suncor.com.android.api.DirectionsApi;
-import suncor.com.android.databinding.FragmentHomeBinding;
+import suncor.com.android.databinding.FragmentHomeGuestBinding;
+import suncor.com.android.databinding.FragmentHomeSignedinBinding;
 import suncor.com.android.di.viewmodel.ViewModelFactory;
-import suncor.com.android.model.DirectionsResult;
-import suncor.com.android.model.Resource;
-import suncor.com.android.model.account.Profile;
 import suncor.com.android.model.station.Station;
 import suncor.com.android.ui.main.BottomNavigationFragment;
 import suncor.com.android.ui.main.stationlocator.StationDetailsDialog;
-import suncor.com.android.ui.main.stationlocator.StationItem;
 import suncor.com.android.utilities.LocationUtils;
 import suncor.com.android.utilities.NavigationAppsHelper;
 import suncor.com.android.utilities.PermissionManager;
@@ -52,9 +48,10 @@ public class HomeFragment extends BottomNavigationFragment {
     private RewardsAdapter rewardsAdapter;
     private LocationLiveData locationLiveData;
     private boolean inAnimationShown;
-    private FragmentHomeBinding binding;
     @Inject
     PermissionManager permissionManager;
+
+    private CardView nearestCard;
 
     private OnClickListener tryAgainLister = v -> {
         if (mViewModel.getUserLocation() != null) {
@@ -63,39 +60,109 @@ public class HomeFragment extends BottomNavigationFragment {
             mViewModel.setLocationServiceEnabled(LocationUtils.isLocationEnabled(getContext()));
         }
     };
-    private OnClickListener openNavigation = v -> {
-        if (mViewModel.stationItem != null) {
-            NavigationAppsHelper.openNavigationApps(getActivity(), mViewModel.stationItem.getStation());
-        }
-    };
-    private OnClickListener showCardDetail = v -> {
-        if (mViewModel.stationItem != null && !mViewModel.isLoading.get()) {
-            StationDetailsDialog.showCard(this, mViewModel.stationItem, binding.stationCard, false);
-        }
-    };
 
-    public static HomeFragment newInstance() {
-        return new HomeFragment();
-    }
+    private OnClickListener showCardDetail = v -> {
+        if (mViewModel.nearestStation.getValue().data != null && !mViewModel.isLoading.get()) {
+            StationDetailsDialog.showCard(this, mViewModel.nearestStation.getValue().data, nearestCard, false);
+        }
+    };
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         locationLiveData = new LocationLiveData(getContext().getApplicationContext());
         mViewModel = ViewModelProviders.of(this, viewModelFactory).get(HomeViewModel.class);
-        mViewModel.nearestStation.observe(this, stationObserver);
+        mViewModel.locationServiceEnabled.observe(this, (enabled -> {
+            if (enabled) {
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED) {
+                    mViewModel.isLoading.set(mViewModel.getUserLocation() == null);
+                    locationLiveData.observe(getViewLifecycleOwner(), (location -> mViewModel.setUserLocation(new LatLng(location.getLatitude(), location.getLongitude()))));
+                }
+            }
+        }));
+        mViewModel.openNavigationApps.observe(this, event -> {
+            Station station = event.getContentIfNotHandled();
+            if (station != null) {
+                NavigationAppsHelper.openNavigationApps(getActivity(), station);
+            }
+        });
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        if (!mViewModel.isUserLoggedIn()) {
+            return setupGuestLayout(inflater, container);
+        } else {
+            return setupSignedInLayout(inflater, container);
+        }
+    }
+
+    private View setupSignedInLayout(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        FragmentHomeSignedinBinding binding = FragmentHomeSignedinBinding.inflate(inflater, container, false);
         binding.setVm(mViewModel);
         binding.setLifecycleOwner(this);
         binding.setIsLoading(mViewModel.isLoading);
         binding.tryAgainButton.setOnClickListener(tryAgainLister);
         binding.stationCard.setOnClickListener(showCardDetail);
-        binding.directionsButton.setOnClickListener(openNavigation);
+        nearestCard = binding.stationCard;
+        rewardsAdapter = new RewardsAdapter(getActivity(), mViewModel);
+        binding.carouselCardRecycler.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        PagerSnapHelper helper = new PagerSnapHelper();
+        helper.attachToRecyclerView(binding.carouselCardRecycler);
+        binding.carouselCardRecycler.setAdapter(rewardsAdapter);
+        return binding.getRoot();
+    }
+
+    private View setupGuestLayout(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        FragmentHomeGuestBinding binding = FragmentHomeGuestBinding.inflate(inflater, container, false);
+        binding.setVm(mViewModel);
+        binding.setLifecycleOwner(this);
+        binding.setIsLoading(mViewModel.isLoading);
+        binding.tryAgainButton.setOnClickListener(tryAgainLister);
+        binding.stationCard.setOnClickListener(showCardDetail);
+        nearestCard = binding.stationCard;
+        rewardsAdapter = new RewardsAdapter(getActivity(), mViewModel);
+        binding.carouselCardRecycler.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        PagerSnapHelper helper = new PagerSnapHelper();
+        helper.attachToRecyclerView(binding.carouselCardRecycler);
+        binding.carouselCardRecycler.setAdapter(rewardsAdapter);
+
+        binding.settingsButton.setOnClickListener(v -> {
+            permissionManager.checkPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION, new PermissionManager.PermissionAskListener() {
+                @Override
+                public void onNeedPermission() {
+                    showRequestLocationDialog(false);
+                }
+
+                @Override
+                public void onPermissionPreviouslyDenied() {
+                    //in case in the future we would show any rational
+                    showRequestLocationDialog(false);
+                }
+
+                @Override
+                public void onPermissionPreviouslyDeniedWithNeverAskAgain() {
+                    showRequestLocationDialog(true);
+                }
+
+                @Override
+                public void onPermissionGranted() {
+                    showRequestLocationDialog(false);
+                }
+            });
+        });
+
+        if (!inAnimationShown && !mViewModel.isUserLoggedIn()) {
+            inAnimationShown = true;
+            Animation animFromLet = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in_right_home);
+            animFromLet.setDuration(500);
+            Animation animslideUp = AnimationUtils.loadAnimation(getContext(), R.anim.push_up_in);
+            animslideUp.setDuration(500);
+            binding.carouselCardRecycler.startAnimation(animFromLet);
+            nearestCard.startAnimation(animslideUp);
+        }
+
         return binding.getRoot();
     }
 
@@ -130,61 +197,9 @@ public class HomeFragment extends BottomNavigationFragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        if (!inAnimationShown) {
-            inAnimationShown = true;
-            Animation animFromLet = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in_right_home);
-            animFromLet.setDuration(500);
-            Animation animslideUp = AnimationUtils.loadAnimation(getContext(), R.anim.push_up_in);
-            animslideUp.setDuration(500);
-            binding.carouselCardRecycler.startAnimation(animFromLet);
-            binding.stationCard.startAnimation(animslideUp);
-        }
+    public void onStart() {
+        super.onStart();
         checkAndRequestPermission();
-
-        rewardsAdapter = new RewardsAdapter(getActivity(), mViewModel);
-        binding.carouselCardRecycler.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
-        PagerSnapHelper helper = new PagerSnapHelper();
-        helper.attachToRecyclerView(binding.carouselCardRecycler);
-        binding.carouselCardRecycler.setAdapter(rewardsAdapter);
-        if (mViewModel.isUserLoggedIn()) {
-            showWelcomeMessage();
-        }
-
-        binding.settingsButton.setOnClickListener(v -> {
-            permissionManager.checkPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION, new PermissionManager.PermissionAskListener() {
-                @Override
-                public void onNeedPermission() {
-
-                    showRequestLocationDialog(false);
-                }
-
-                @Override
-                public void onPermissionPreviouslyDenied() {
-                    //in case in the future we would show any rational 
-                    showRequestLocationDialog(false);
-                }
-
-                @Override
-                public void onPermissionPreviouslyDeniedWithNeverAskAgain() {
-                    showRequestLocationDialog(true);
-                }
-
-                @Override
-                public void onPermissionGranted() {
-                    showRequestLocationDialog(false);
-                }
-            });
-        });
-        mViewModel.locationServiceEnabled.observe(this, (enabled -> {
-            if (enabled) {
-                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED) {
-                    mViewModel.isLoading.set(mViewModel.getUserLocation() == null);
-                    locationLiveData.observe(getViewLifecycleOwner(), (location -> mViewModel.setUserLocation(new LatLng(location.getLatitude(), location.getLongitude()))));
-                }
-            }
-        }));
     }
 
     @Override
@@ -202,19 +217,13 @@ public class HomeFragment extends BottomNavigationFragment {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    private void showWelcomeMessage() {
-        //TODO improve this
-        binding.welcomeLayout.setVisibility(View.VISIBLE);
-        Profile profile = mViewModel.getProfile();
-        binding.welcomeMessage.setText(getString(R.string.dashboard_enrolled_welcome_message, profile.getFirstName(), mViewModel.getRewardedPoints()));
-    }
-
     @Override
     public void onLoginStatusChanged() {
-        if (mViewModel.isUserLoggedIn()) {
-            showWelcomeMessage();
-        } else {
-            binding.welcomeLayout.setVisibility(View.GONE);
+        if (!this.isDetached()) {
+            getFragmentManager().beginTransaction()
+                    .detach(this)
+                    .attach(this)
+                    .commit();
         }
     }
 
@@ -226,30 +235,6 @@ public class HomeFragment extends BottomNavigationFragment {
 
         }
     }
-
-    Observer<Resource<Station>> stationObserver = resource -> {
-        if (resource.status == Resource.Status.SUCCESS) {
-            if (resource.data != null) {
-                Station station = resource.data;
-                StationItem stationItem = new StationItem(station);
-                mViewModel.stationItem = stationItem;
-                binding.setStation(stationItem);
-                if (stationItem.getDistanceDuration() == null) {
-                    LatLng dest = new LatLng(station.getAddress().getLatitude(), station.getAddress().getLongitude());
-                    LatLng origin = new LatLng(mViewModel.getUserLocation().latitude, mViewModel.getUserLocation().longitude);
-                    DirectionsApi.getInstance().enqueuJob(origin, dest)
-                            .observe(getViewLifecycleOwner(), result -> {
-                                if (result.status == Resource.Status.SUCCESS) {
-                                    mViewModel.stationItem.setDistanceDuration(result.data);
-                                } else if (result.status == Resource.Status.ERROR) {
-                                    mViewModel.stationItem.setDistanceDuration(DirectionsResult.INVALID);
-                                }
-                            });
-                }
-            }
-        }
-    };
-
 
     private void checkAndRequestPermission() {
         permissionManager.checkPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION, new PermissionManager.PermissionAskListener() {
--- app/src/main/java/suncor/com/android/ui/main/home/HomeViewModel.java
+++ app/src/main/java/suncor/com/android/ui/main/home/HomeViewModel.java
@@ -13,8 +13,10 @@ import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
+import suncor.com.android.api.DirectionsApi;
 import suncor.com.android.data.repository.stations.StationsProvider;
 import suncor.com.android.mfp.SessionManager;
+import suncor.com.android.model.DirectionsResult;
 import suncor.com.android.model.Resource;
 import suncor.com.android.model.account.Profile;
 import suncor.com.android.model.station.Station;
@@ -26,15 +28,17 @@ public class HomeViewModel extends ViewModel {
 
     private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(new LatLng(20, -180), new LatLng(90, -50));
     private final static int DISTANCE_API = 25000;
-    public StationItem stationItem;
     public ObservableBoolean isLoading = new ObservableBoolean(false);
-    private MediatorLiveData<Resource<Station>> _nearestStation = new MediatorLiveData<>();
-    public LiveData<Resource<Station>> nearestStation = _nearestStation;
+    private MediatorLiveData<Resource<StationItem>> _nearestStation = new MediatorLiveData<>();
+    public LiveData<Resource<StationItem>> nearestStation = _nearestStation;
     private MutableLiveData<Boolean> _locationServiceEnabled = new MutableLiveData<>();
     public LiveData<Boolean> locationServiceEnabled = _locationServiceEnabled;
     private MutableLiveData<Event<Boolean>> loadNearest = new MutableLiveData<>();
     private SessionManager sessionManager;
     private LatLng userLocation;
+
+    private MutableLiveData<Event<Station>> _openNavigationApps = new MutableLiveData<>();
+    public LiveData<Event<Station>> openNavigationApps = _openNavigationApps;
 
     @Inject
     public HomeViewModel(SessionManager sessionManager, StationsProvider stationsProvider) {
@@ -63,11 +67,34 @@ public class HomeViewModel extends ViewModel {
                     if (resource.data == null || resource.data.isEmpty()) {
                         _nearestStation.setValue(Resource.success(null));
                     } else {
-                        _nearestStation.setValue(Resource.success(resource.data.get(0)));
+                        _nearestStation.setValue(Resource.success(new StationItem(resource.data.get(0))));
                     }
                     break;
             }
         }));
+
+        LiveData<Resource<DirectionsResult>> directionsResultLiveData = Transformations.switchMap(_nearestStation, resource -> {
+            if (resource.status == Resource.Status.SUCCESS && resource.data.getDistanceDuration() == null) {
+                LatLng dest = new LatLng(resource.data.getStation().getAddress().getLatitude(), resource.data.getStation().getAddress().getLongitude());
+                LatLng origin = new LatLng(userLocation.latitude, userLocation.longitude);
+                return DirectionsApi.getInstance().enqueuJob(origin, dest);
+            } else {
+                return new MutableLiveData<>();
+            }
+        });
+
+        directionsResultLiveData.observeForever(resource -> {
+            if (resource.status == Resource.Status.LOADING) {
+                return;
+            }
+            StationItem item = nearestStation.getValue().data;
+            if (resource.status == Resource.Status.SUCCESS) {
+                item.setDistanceDuration(resource.data);
+            } else if (resource.status == Resource.Status.ERROR) {
+                item.setDistanceDuration(DirectionsResult.INVALID);
+            }
+            _nearestStation.setValue(Resource.success(item));
+        });
     }
 
 
@@ -106,6 +133,11 @@ public class HomeViewModel extends ViewModel {
                 isLoading.set(false);
             }
         }
+    }
+
+    public void openNavigationApps() {
+        Station nearestStation = _nearestStation.getValue().data.getStation();
+        _openNavigationApps.setValue(Event.newEvent(nearestStation));
     }
 
     public Profile getProfile() {
