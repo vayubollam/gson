b95c46076ca79b3bdf3d84be6a173b945fea7495
hicham boushaba <hicham.boushaba@outlook.com>
2019-06-28 11:43:58 +0100

    Navigate to home when dismissing error alert on login.

--- app/src/main/java/suncor/com/android/ui/login/LoginViewModel.java
+++ app/src/main/java/suncor/com/android/ui/login/LoginViewModel.java
@@ -1,11 +1,12 @@
 package suncor.com.android.ui.login;
 
-import javax.inject.Inject;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
+
+import javax.inject.Inject;
+
 import suncor.com.android.BR;
 import suncor.com.android.R;
 import suncor.com.android.mfp.SessionManager;
@@ -26,6 +27,7 @@ public class LoginViewModel extends ViewModel {
     private MutableLiveData<Event<Boolean>> passwordResetEvent = new MutableLiveData<>();
     private MutableLiveData<Event<Boolean>> callCustomerService = new MutableLiveData<>();
     private MutableLiveData<Event<String>> createPasswordEvent = new MutableLiveData<>();
+    private MutableLiveData<Event<Boolean>> navigateToHomeEvent = new MutableLiveData<>();
     private boolean isLoginFromEnrollment;
 
     @Inject
@@ -84,10 +86,13 @@ public class LoginViewModel extends ViewModel {
                         )));
                         break;
                     case OTHER_FAILURE:
-                        loginFailedEvent.postValue(Event.newEvent(new LoginFailResponse(
+                        LoginFailResponse failResponse = new LoginFailResponse(
                                 R.string.msg_e001_title,
                                 new ErrorMessage(R.string.msg_e001_message)
-                        )));
+
+                        );
+                        failResponse.positiveButtonCallback = () -> navigateToHomeEvent.postValue(Event.newEvent(true));
+                        loginFailedEvent.postValue(Event.newEvent(failResponse));
                         break;
                     case PASSWORD_RESET:
                         createPasswordEvent.postValue(Event.newEvent(response.getAdditionalData()));
@@ -126,6 +131,10 @@ public class LoginViewModel extends ViewModel {
 
     public LiveData<Event<LoginFailResponse>> getLoginFailedEvent() {
         return loginFailedEvent;
+    }
+
+    public LiveData<Event<Boolean>> getNavigateToHomeEvent() {
+        return navigateToHomeEvent;
     }
 
     public LiveData<Boolean> getIsLoading() {
@@ -170,14 +179,16 @@ public class LoginViewModel extends ViewModel {
     public static class LoginFailResponse {
         int title;
         ErrorMessage message;
-        int buttonTitle;
-        ErrorAlertCallback callback;
+        int negativeButtonTitle;
+        ErrorAlertCallback negativeButtonCallBack;
+        int positiveButtonTitle = R.string.ok;
+        ErrorAlertCallback positiveButtonCallback;
 
-        public LoginFailResponse(int title, ErrorMessage message, int buttonTitle, ErrorAlertCallback callback) {
+        public LoginFailResponse(int title, ErrorMessage message, int negativeButtonTitle, ErrorAlertCallback negativeButtonCallBack) {
             this.title = title;
             this.message = message;
-            this.callback = callback;
-            this.buttonTitle = buttonTitle;
+            this.negativeButtonCallBack = negativeButtonCallBack;
+            this.negativeButtonTitle = negativeButtonTitle;
         }
 
         public LoginFailResponse(int title, ErrorMessage message) {
