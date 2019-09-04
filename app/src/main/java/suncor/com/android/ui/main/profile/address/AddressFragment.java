a1fd4d02ceed052fc47fc42227d9da71389b1127
hicham rafia <hicham.rafia@ibm.com>
2019-09-04 16:36:30 +0100

    Change address. -wip

---
+++ app/src/main/java/suncor/com/android/ui/main/profile/address/AddressFragment.java
@@ -0,0 +1,36 @@
+package suncor.com.android.ui.main.profile.address;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import suncor.com.android.R;
+
+public class AddressFragment extends Fragment {
+
+    private AddressViewModel mViewModel;
+
+    public static AddressFragment newInstance() {
+        return new AddressFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_address, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(AddressViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/suncor/com/android/ui/main/profile/address/AddressViewModel.java
@@ -0,0 +1,7 @@
+package suncor.com.android.ui.main.profile.address;
+
+import androidx.lifecycle.ViewModel;
+
+public class AddressViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
