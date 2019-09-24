74f91c9368e763f63b0e6a4ee109a11d807c4e29
Hicham.Rafia <hicham.rafia@ibm.com>
2019-09-24 15:12:25 +0100

    Handle merchant name translation.

--- app/src/main/java/suncor/com/android/ui/main/rewards/MerchantItem.java
+++ app/src/main/java/suncor/com/android/ui/main/rewards/MerchantItem.java
@@ -1,13 +1,18 @@
 package suncor.com.android.ui.main.rewards;
 
 
+import android.content.Context;
+
+import suncor.com.android.R;
 import suncor.com.android.model.merchants.Merchant;
 
 public class MerchantItem {
     private Merchant merchant;
+    private Context context;
 
-    public MerchantItem(Merchant merchant) {
+    public MerchantItem(Merchant merchant, Context context) {
         this.merchant = merchant;
+        this.context = context;
     }
 
     String getMerchantLargeImage() {
@@ -42,6 +47,24 @@ public class MerchantItem {
             case MerchantsIds.WINNERS_HomeSense_Marshalls_EN:
             case MerchantsIds.WINNERS_HomeSense_Marshalls_FR:
                 return "winners_small";
+        }
+        return null;
+    }
+
+    public String getLocalizedMerchantName() {
+        switch (merchant.getMerchantId()) {
+            case MerchantsIds.Cara_EN:
+            case MerchantsIds.Cara_FR:
+                return context.getResources().getString(R.string.merchant_dining_card);
+            case MerchantsIds.Cineplex_EN:
+            case MerchantsIds.Cineplex_FR:
+                return context.getResources().getString(R.string.merchant_cineplex);
+            case MerchantsIds.Hudson_Bay_EN:
+            case MerchantsIds.Hudson_Bay_FR:
+                return context.getResources().getString(R.string.merchant_Hudson_bay);
+            case MerchantsIds.WINNERS_HomeSense_Marshalls_EN:
+            case MerchantsIds.WINNERS_HomeSense_Marshalls_FR:
+                return context.getResources().getString(R.string.merchant_winners);
         }
         return null;
     }
