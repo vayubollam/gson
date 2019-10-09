5c58636d16aa797bb08ebc9266bc451b92d650d6
Hicham.Rafia <hicham.rafia@ibm.com>
2019-10-09 15:45:56 +0100

    Remove email check from update Personnel info.

--- app/src/main/java/suncor/com/android/data/account/EnrollmentsApi.java
+++ app/src/main/java/suncor/com/android/data/account/EnrollmentsApi.java
@@ -1,10 +1,9 @@
 package suncor.com.android.data.account;
 
+import androidx.lifecycle.LiveData;
+
 import java.util.ArrayList;
 
-import javax.annotation.Nullable;
-
-import androidx.lifecycle.LiveData;
 import suncor.com.android.model.Resource;
 import suncor.com.android.model.account.CardStatus;
 import suncor.com.android.model.account.NewEnrollment;
@@ -13,14 +12,8 @@ import suncor.com.android.model.account.SecurityQuestion;
 public interface EnrollmentsApi {
     LiveData<Resource<Integer>> registerAccount(NewEnrollment account);
 
-    LiveData<Resource<EmailState>> checkEmail(String email, @Nullable String petroCanadaId);
-
     LiveData<Resource<ArrayList<SecurityQuestion>>> fetchSecurityQuestions();
 
     LiveData<Resource<CardStatus>> checkCardStatus(String cardNumber, String postalCode, String lastName);
 
-
-    enum EmailState {
-        VALID, ALREADY_REGISTERED, RESTRICTED, UNCHECKED
-    }
 }
