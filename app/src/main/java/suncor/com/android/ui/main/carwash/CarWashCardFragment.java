d30087ec35982008a1bbe69a627063a7b50eaf2f
Liam Lu <liam.lu@ibm.com>
2019-10-17 12:00:38 -0400

    Add location card basic functions.

--- app/src/main/java/suncor/com/android/ui/main/carwash/CarWashCardFragment.java
+++ app/src/main/java/suncor/com/android/ui/main/carwash/CarWashCardFragment.java
@@ -1,5 +1,8 @@
 package suncor.com.android.ui.main.carwash;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Outline;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
@@ -14,19 +17,25 @@ import android.view.ViewOutlineProvider;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.widget.NestedScrollView;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.ArrayList;
 
 import javax.inject.Inject;
 
+import suncor.com.android.LocationLiveData;
 import suncor.com.android.R;
 import suncor.com.android.databinding.FragmentCarWashBinding;
 import suncor.com.android.di.viewmodel.ViewModelFactory;
 import suncor.com.android.model.cards.CardDetail;
+import suncor.com.android.model.station.Station;
 import suncor.com.android.ui.common.GenericErrorView;
 import suncor.com.android.ui.common.OnBackPressedListener;
 import suncor.com.android.ui.main.cards.list.CardItemDecorator;
@@ -34,9 +43,17 @@ import suncor.com.android.ui.main.cards.list.CardListItem;
 import suncor.com.android.ui.main.cards.list.CardsListAdapter;
 import suncor.com.android.ui.main.common.MainActivityFragment;
 import suncor.com.android.uicomponents.swiperefreshlayout.SwipeRefreshLayout;
+import suncor.com.android.utilities.LocationUtils;
+import suncor.com.android.utilities.NavigationAppsHelper;
+import suncor.com.android.utilities.PermissionManager;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class CarWashCardFragment extends MainActivityFragment implements OnBackPressedListener,
         SwipeRefreshLayout.OnRefreshListener {
+
+    private static final int REQUEST_CHECK_SETTINGS = 100;
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Inject
     ViewModelFactory viewModelFactory;
@@ -44,6 +61,10 @@ public class CarWashCardFragment extends MainActivityFragment implements OnBackP
     private CarWashCardViewModel viewModel;
     private CardsListAdapter petroCanadaCardsAdapter;
     private float appBarElevation;
+
+    private LocationLiveData locationLiveData;
+    @Inject
+    PermissionManager permissionManager;
 
 
     @Override
@@ -69,6 +90,16 @@ public class CarWashCardFragment extends MainActivityFragment implements OnBackP
                 petroCanadaCardsAdapter.setCards(petroCanadaCards);
             }
         });
+
+        locationLiveData = new LocationLiveData(getContext().getApplicationContext());
+        viewModel.locationServiceEnabled.observe(this, (enabled -> {
+            if (enabled) {
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED) {
+                    viewModel.isLoading.set(viewModel.getUserLocation() == null);
+                    locationLiveData.observe(getViewLifecycleOwner(), (location -> viewModel.setUserLocation(new LatLng(location.getLatitude(), location.getLongitude()))));
+                }
+            }
+        }));
 
     }
 
@@ -122,6 +153,11 @@ public class CarWashCardFragment extends MainActivityFragment implements OnBackP
             }
         });
 
+        //Setup nearest card click listeners
+        binding.carwashNearestCards.tryAgainButton.setOnClickListener(tryAgainLister);
+        binding.carwashNearestCards.directionsButton.setOnClickListener(openNavigationListener);
+        binding.carwashNearestCards.settingsButton.setOnClickListener(openSettingListener);
+
         return binding.getRoot();
 
     }
@@ -130,6 +166,7 @@ public class CarWashCardFragment extends MainActivityFragment implements OnBackP
     public void onStart() {
         super.onStart();
         viewModel.onAttached();
+        checkAndRequestPermission();
     }
 
     /**
@@ -154,5 +191,117 @@ public class CarWashCardFragment extends MainActivityFragment implements OnBackP
         //TODO: goto card detail page
     }
 
+    private View.OnClickListener tryAgainLister = v -> {
+        if (viewModel.getUserLocation() != null) {
+            viewModel.setUserLocation(viewModel.getUserLocation());
+        } else {
+            viewModel.setLocationServiceEnabled(LocationUtils.isLocationEnabled(getContext()));
+        }
+    };
 
+    private View.OnClickListener openNavigationListener = v -> {
+        if (viewModel.getNearestStation().getValue() != null) {
+            Station station = viewModel.getNearestStation().getValue().data.getStation();
+            if (station != null) {
+                NavigationAppsHelper.openNavigationApps(getActivity(), station);
+            }
+        }
+    };
+
+    private View.OnClickListener openSettingListener = v -> permissionManager.checkPermission(getContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION, new PermissionManager.PermissionAskListener() {
+                @Override
+                public void onNeedPermission() {
+                    showRequestLocationDialog(false);
+                }
+
+                @Override
+                public void onPermissionPreviouslyDenied() {
+                    //in case in the future we would show any rational
+                    showRequestLocationDialog(false);
+                }
+
+                @Override
+                public void onPermissionPreviouslyDeniedWithNeverAskAgain() {
+                    showRequestLocationDialog(true);
+                }
+
+                @Override
+                public void onPermissionGranted() {
+                    showRequestLocationDialog(false);
+                }
+            });
+
+    private void showRequestLocationDialog(boolean previouselyDeniedWithNeverASk) {
+        AlertDialog.Builder adb = new AlertDialog.Builder(getContext());
+        adb.setTitle(R.string.enable_location_dialog_title);
+        adb.setMessage(R.string.enable_location_dialog_message);
+        adb.setNegativeButton(R.string.cancel, null);
+        adb.setPositiveButton(R.string.ok, (dialog, which) -> {
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_GRANTED && !LocationUtils.isLocationEnabled(getContext())) {
+                LocationUtils.openLocationSettings(this, REQUEST_CHECK_SETTINGS);
+                return;
+            }
+
+            permissionManager.setFirstTimeAsking(Manifest.permission.ACCESS_FINE_LOCATION, false);
+            if (previouselyDeniedWithNeverASk) {
+                PermissionManager.openAppSettings(getActivity());
+            } else {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE);
+            }
+            dialog.dismiss();
+        });
+        AlertDialog alertDialog = adb.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        alertDialog.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults[0] == PERMISSION_GRANTED) {
+                if (LocationUtils.isLocationEnabled(getContext())) {
+                    viewModel.setLocationServiceEnabled(true);
+                } else {
+                    LocationUtils.openLocationSettings(this, REQUEST_CHECK_SETTINGS);
+                }
+
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CHECK_SETTINGS && resultCode == Activity.RESULT_OK) {
+            viewModel.setLocationServiceEnabled(true);
+
+        }
+    }
+
+    private void checkAndRequestPermission() {
+        permissionManager.checkPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION, new PermissionManager.PermissionAskListener() {
+            @Override
+            public void onNeedPermission() {
+                if (!permissionManager.isAlertShown()) {
+                    permissionManager.setAlertShown(true);
+                    showRequestLocationDialog(false);
+                }
+            }
+
+            @Override
+            public void onPermissionPreviouslyDenied() {
+                //in case in the future we would show any rational
+            }
+
+            @Override
+            public void onPermissionPreviouslyDeniedWithNeverAskAgain() {
+            }
+
+            @Override
+            public void onPermissionGranted() {
+                viewModel.setLocationServiceEnabled(LocationUtils.isLocationEnabled(getContext()));
+            }
+        });
+    }
 }
--- app/src/main/java/suncor/com/android/ui/main/carwash/CarWashCardViewModel.java
+++ app/src/main/java/suncor/com/android/ui/main/carwash/CarWashCardViewModel.java
@@ -1,25 +1,42 @@
 package suncor.com.android.ui.main.carwash;
 
+import android.util.Log;
+
+import androidx.databinding.ObservableBoolean;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import suncor.com.android.data.DistanceApi;
 import suncor.com.android.data.cards.CardsRepository;
+import suncor.com.android.data.favourite.FavouriteRepository;
+import suncor.com.android.data.stations.StationsApi;
+import suncor.com.android.model.DirectionsResult;
 import suncor.com.android.model.Resource;
 import suncor.com.android.model.cards.CardDetail;
 import suncor.com.android.model.cards.CardType;
+import suncor.com.android.model.station.Station;
 import suncor.com.android.ui.common.Event;
+import suncor.com.android.ui.main.stationlocator.StationItem;
+import suncor.com.android.utilities.LocationUtils;
+import suncor.com.android.utilities.NavigationAppsHelper;
 
 public class CarWashCardViewModel extends ViewModel {
 
     private final CardsRepository repository;
+    private FavouriteRepository favouriteRepository;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(new LatLng(20, -180), new LatLng(90, -50));
+    private final static int DISTANCE_API = 25000;
 
     private MutableLiveData<ViewState> viewState = new MutableLiveData<>();
     private MutableLiveData<Boolean> isBalanceZero = new MutableLiveData<>();
@@ -28,19 +45,33 @@ public class CarWashCardViewModel extends ViewModel {
     private MutableLiveData<List<CardDetail>> petroCanadaCards = new MutableLiveData<>();
     private MutableLiveData<Event<Boolean>> refreshCardsEvent = new MutableLiveData<>();
 
+    public ObservableBoolean isLoading = new ObservableBoolean(false);
+    private MediatorLiveData<Resource<StationItem>> _nearestStation = new MediatorLiveData<>();
+    public LiveData<Resource<StationItem>> nearestStation = _nearestStation;
+
+
+    private MutableLiveData<Boolean> _locationServiceEnabled = new MutableLiveData<>();
+    public LiveData<Boolean> locationServiceEnabled = _locationServiceEnabled;
+    private MutableLiveData<Event<Boolean>> loadNearest = new MutableLiveData<>();
+    private LatLng userLocation;
+
+
     @Inject
-    public CarWashCardViewModel(CardsRepository repository) {
+    public CarWashCardViewModel(CardsRepository repository, StationsApi stationsApi, FavouriteRepository favouriteRepository, DistanceApi distanceApi) {
         this.repository = repository;
+        this.favouriteRepository = favouriteRepository;
 
         //TODO: merge single ticket live data
         MediatorLiveData<Resource<ArrayList<CardDetail>>> apiCall = new MediatorLiveData<>();
+        //load wash cards
         LiveData<Resource<ArrayList<CardDetail>>> retrieveCall = Transformations.switchMap(retrieveCardsEvent, event -> {
             if (event.getContentIfNotHandled() != null) {
-                return repository.getCards(true);
+                return repository.getCards(false);
             }
             return new MutableLiveData<>();
         });
 
+        //refresh wash cards
         LiveData<Resource<ArrayList<CardDetail>>> refreshCall = Transformations.switchMap(refreshCardsEvent, event -> {
             if (event.getContentIfNotHandled() != null) {
                 return repository.getCards(true);
@@ -63,6 +94,58 @@ public class CarWashCardViewModel extends ViewModel {
             }
         });
 
+        //load near stations
+        LiveData<Resource<ArrayList<Station>>> nearestStationLoad = Transformations.switchMap(loadNearest, event -> {
+            if (event.getContentIfNotHandled() != null) {
+                LatLngBounds bounds = LocationUtils.calculateSquareBounds(userLocation, DISTANCE_API);
+                return stationsApi.getStations(bounds, true);
+            } else {
+                return new MutableLiveData<>();
+            }
+        });
+
+        _nearestStation.addSource(nearestStationLoad, ((resource) -> {
+            isLoading.set(resource.status == Resource.Status.LOADING);
+            switch (resource.status) {
+                case LOADING:
+                    _nearestStation.setValue(Resource.loading());
+                    break;
+                case ERROR:
+                    _nearestStation.setValue(Resource.error(resource.message));
+                    break;
+                case SUCCESS:
+                    if (resource.data == null || resource.data.isEmpty()) {
+                        _nearestStation.setValue(Resource.success(null));
+                    } else {
+                        _nearestStation.setValue(Resource.success(new StationItem(favouriteRepository, resource.data.get(0), favouriteRepository.isFavourite(resource.data.get(0)))));
+                    }
+                    break;
+            }
+        }));
+
+        LiveData<Resource<DirectionsResult>> directionsResultLiveData = Transformations.switchMap(_nearestStation, resource -> {
+            if (resource.status == Resource.Status.SUCCESS && resource.data != null && resource.data.getDistanceDuration() == null) {
+                LatLng dest = new LatLng(resource.data.getStation().getAddress().getLatitude(), resource.data.getStation().getAddress().getLongitude());
+                LatLng origin = new LatLng(userLocation.latitude, userLocation.longitude);
+                return distanceApi.enqueuJob(origin, dest);
+            } else {
+                return new MutableLiveData<>();
+            }
+        });
+
+        directionsResultLiveData.observeForever(resource -> {
+            if (resource.status == Resource.Status.LOADING) {
+                return;
+            }
+            StationItem item = nearestStation.getValue().data;
+            if (resource.status == Resource.Status.SUCCESS) {
+                item.setDistanceDuration(resource.data);
+            } else if (resource.status == Resource.Status.ERROR) {
+                item.setDistanceDuration(DirectionsResult.INVALID);
+            }
+            _nearestStation.setValue(Resource.success(item));
+        });
+
     }
 
     public void onAttached() {
@@ -72,6 +155,7 @@ public class CarWashCardViewModel extends ViewModel {
     public void loadData(ViewState state) {
         viewState.setValue(state);
         retrieveCardsEvent.setValue(Event.newEvent(true));
+        if (state == ViewState.REFRESHING) loadNearest.setValue(Event.newEvent(true));
     }
 
     public MutableLiveData<ViewState> getViewState() {
@@ -117,6 +201,39 @@ public class CarWashCardViewModel extends ViewModel {
 
     public MutableLiveData<Boolean> getIsBalanceZero() {
         return isBalanceZero;
+    }
+
+
+    public void setLocationServiceEnabled(boolean enabled) {
+        _locationServiceEnabled.setValue(enabled);
+    }
+
+    public LatLng getUserLocation() {
+        return userLocation;
+    }
+
+    public void setUserLocation(LatLng userLocation) {
+        if (this.userLocation != null && LocationUtils.calculateDistance(this.userLocation, userLocation) < 10) {
+            this.userLocation = userLocation;
+
+            if (!LAT_LNG_BOUNDS.contains(userLocation)) {
+                _nearestStation.setValue(Resource.success(null));
+            } else if (_nearestStation.getValue() != null && _nearestStation.getValue().status != Resource.Status.SUCCESS) {
+                loadNearest.setValue(Event.newEvent(true));
+            }
+        } else {
+            this.userLocation = userLocation;
+            if (LAT_LNG_BOUNDS.contains(userLocation)) {
+                loadNearest.setValue(Event.newEvent(true));
+            } else {
+                _nearestStation.setValue(Resource.success(null));
+                isLoading.set(false);
+            }
+        }
+    }
+
+    public LiveData<Resource<StationItem>> getNearestStation() {
+        return nearestStation;
     }
 
     public enum ViewState {
