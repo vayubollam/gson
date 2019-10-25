d807c3fd739accfdefe9751edcb565279d352625
Liam Lu <liam.lu@ibm.com>
2019-10-25 14:12:04 -0600

    Add static barcode generator.

--- app/src/main/java/suncor/com/android/ui/main/carwash/CarWashBarCodeFragment.java
+++ app/src/main/java/suncor/com/android/ui/main/carwash/CarWashBarCodeFragment.java
@@ -1,6 +1,10 @@
 package suncor.com.android.ui.main.carwash;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +14,11 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 import suncor.com.android.R;
 import suncor.com.android.databinding.FragmentCarwashBarcodeBinding;
@@ -52,6 +61,7 @@ public class CarWashBarCodeFragment extends MainActivityFragment implements OnBa
         binding.reEnterButton.setOnClickListener(v -> {
             goBack(true);
         });
+        binding.barCodeImage.setImageBitmap(generateBarcode("123456789012"));
         return binding.getRoot();
     }
 
@@ -63,5 +73,28 @@ public class CarWashBarCodeFragment extends MainActivityFragment implements OnBa
     @Override
     public void onBackPressed() {
         goBack(false);
+    }
+
+    private Bitmap generateBarcode(String barCodeNumber) {
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        Resources r = getResources();
+        int width = Math.round(TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, r.getDimension(R.dimen.carwash_barcode_image_width), r.getDisplayMetrics()));
+        int height = Math.round(TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, r.getDimension(R.dimen.carwash_barcode_image_height), r.getDisplayMetrics()));
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(barCodeNumber, BarcodeFormat.EAN_13, width, height);
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    bitmap.setPixel(i, j, bitMatrix.get(i, j) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            return bitmap;
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
