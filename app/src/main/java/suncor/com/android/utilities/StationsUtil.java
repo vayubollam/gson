3521554eca9ff0f7419423d1cbbf917def737a1a
Liam Lu <liam.lu@ibm.com>
2019-11-19 10:24:31 -0700

    Make inde alert not cancelable.

--- app/src/main/java/suncor/com/android/utilities/StationsUtil.java
+++ app/src/main/java/suncor/com/android/utilities/StationsUtil.java
@@ -1,5 +1,6 @@
 package suncor.com.android.utilities;
 
+import android.app.Dialog;
 import android.content.Context;
 
 import androidx.appcompat.app.AlertDialog;
@@ -11,11 +12,14 @@ import suncor.com.android.model.station.Station;
 
 public class StationsUtil {
     public static void showIndependentStationAlert(Context context) {
-        new AlertDialog.Builder(context)
+        Dialog dialog = new AlertDialog.Builder(context)
                 .setTitle(context.getString(R.string.carwash_independent_alert_title))
                 .setMessage(context.getString(R.string.carwash_independent_alert_message))
                 .setPositiveButton(context.getString(R.string.ok), null)
-                .show();
+                .setCancelable(false)
+                .create();
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
     }
 
     public static Station filterNearestCarWashStation(List<Station> stations) {
