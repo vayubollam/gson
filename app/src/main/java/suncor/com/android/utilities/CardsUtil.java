ab6663e70b9976213318f8c5efad9665134cc4c6
Liam Lu <liam.lu@ibm.com>
2019-12-11 22:41:26 -0700

    Update zero alert.

--- app/src/main/java/suncor/com/android/utilities/CardsUtil.java
+++ app/src/main/java/suncor/com/android/utilities/CardsUtil.java
@@ -3,6 +3,9 @@ package suncor.com.android.utilities;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 
@@ -15,23 +18,22 @@ public class CardsUtil {
         if (negListener == null) {
             dialog = new AlertDialog.Builder(context)
                     .setTitle(R.string.zero_balance_alert_title)
-                    .setMessage(R.string.zero_balance_alert_message)
-                    .setPositiveButton(R.string.zero_balance_alert_buy, posListener)
-                    .setNegativeButton(R.string.cancel, null)
+                    .setMessage(Html.fromHtml(context.getString(R.string.zero_balance_alert_message), Html.FROM_HTML_MODE_LEGACY))
+                    .setPositiveButton(R.string.cancel, null)
                     .setCancelable(false)
                     .create();
         } else {
             dialog = new AlertDialog.Builder(context)
                     .setTitle(R.string.zero_balance_alert_title)
-                    .setMessage(R.string.zero_balance_alert_message)
-                    .setPositiveButton(R.string.zero_balance_alert_buy, posListener)
-                    .setNegativeButton(R.string.zero_balance_alert_view, negListener)
-                    .setNeutralButton(R.string.cancel, null)
+                    .setMessage(Html.fromHtml(context.getText(R.string.zero_balance_alert_message).toString(), Html.FROM_HTML_MODE_LEGACY))
+                    .setPositiveButton(R.string.zero_balance_alert_view, negListener)
+                    .setNegativeButton(R.string.cancel, null)
                     .setCancelable(false)
                     .create();
         }
         dialog.setCanceledOnTouchOutside(false);
         dialog.show();
+        ((TextView)dialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public static void showOtherCardAvailableAlert(Context context) {
