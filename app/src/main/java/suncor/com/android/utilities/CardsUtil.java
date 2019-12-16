f8969bf19e06f85c11f6ebfe54279319b5e6c829
Liam Lu <liam.lu@ibm.com>
2019-12-15 18:44:40 -0700

    Code clean up.

--- app/src/main/java/suncor/com/android/utilities/CardsUtil.java
+++ app/src/main/java/suncor/com/android/utilities/CardsUtil.java
@@ -11,8 +11,26 @@ import suncor.com.android.R;
 public class CardsUtil {
     public static void showZeroBalanceAlert(Context context, DialogInterface.OnClickListener posListener,
                                             DialogInterface.OnClickListener negListener) {
-        //TODO: posListener is for single ticket redeem flow in the future
         Dialog dialog;
+        //TODO: UNCOMMENT WHEN REDEEM/BUG SINGLE TICKET IS BACK
+//        if (negListener == null) {
+//            dialog = new AlertDialog.Builder(context)
+//                    .setTitle(R.string.zero_balance_alert_title)
+//                    .setMessage(R.string.zero_balance_alert_message)
+//                    .setPositiveButton(R.string.zero_balance_alert_buy, posListener)
+//                    .setNegativeButton(R.string.cancel, null)
+//                    .setCancelable(false)
+//                    .create();
+//        } else {
+//            dialog = new AlertDialog.Builder(context)
+//                    .setTitle(R.string.zero_balance_alert_title)
+//                    .setMessage(R.string.zero_balance_alert_message)
+//                    .setPositiveButton(R.string.zero_balance_alert_buy,posListener)
+//                    .setNegativeButton(R.string.zero_balance_alert_view, negListener)
+//                    .setNeutralButton(R.string.cancel, null)
+//                    .setCancelable(false)
+//                    .create();
+//        }
         if (negListener == null) {
             dialog = new AlertDialog.Builder(context)
                     .setTitle(R.string.zero_balance_alert_title)
