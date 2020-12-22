564717fb126fb3cb0651864cc3fec3a4460a85c7
Asma Khan <asmkhan@suncor.com>
2020-12-22 22:10:30 +0530

    update fetaure tours

--- app/src/main/java/suncor/com/android/ui/tutorial/TutorialFragment.java
+++ app/src/main/java/suncor/com/android/ui/tutorial/TutorialFragment.java
@@ -55,6 +55,7 @@ public class TutorialFragment extends Fragment {
 
     private List<TutorialContent> setUpTutorialSlides(String uriPrefix) {
         List<TutorialContent> tutorialContents = new ArrayList<>();
+        tutorialContents.add(new TutorialContent(getString(R.string.tutorial_silde_page4_header), uriPrefix + R.raw.tutorial_4));
         tutorialContents.add(new TutorialContent(getString(R.string.tutorial_silde_page1_header), uriPrefix + R.raw.tutorial_1));
         tutorialContents.add(new TutorialContent(getString(R.string.tutorial_silde_page2_header), uriPrefix + R.raw.tutorial_2));
         tutorialContents.add(new TutorialContent(getString(R.string.tutorial_silde_page3_header), uriPrefix + R.raw.tutorial_3));
