8ca6612ce25ffe1b977a48e0a326bedf7fe01326
ayush <ayush@ayushs-MacBook-Pro.local>
2021-02-09 23:32:53 -0500

    fixed defect 4319

--- app/src/main/java/suncor/com/android/ui/enrollment/form/EnrollmentFormFragment.java
+++ app/src/main/java/suncor/com/android/ui/enrollment/form/EnrollmentFormFragment.java
@@ -127,6 +127,7 @@ public class EnrollmentFormFragment extends BaseFragment implements OnBackPresse
                     optionsChecked += binding.smsOffersCheckbox.getText().toString();
                 }
 
+                binding.emailAddress.setText(viewModel.getEmailInputField().getText());
                 AnalyticsUtils.logEvent(
                         getContext(),
                         "form_complete",
@@ -157,6 +158,7 @@ public class EnrollmentFormFragment extends BaseFragment implements OnBackPresse
                                 new Pair<>("formName",  formName)
                         );
                         binding.emailInput.setText("");
+                        viewModel.getEmailInputField().setText("");
                         d.dismiss();
                         focusOnItem(binding.emailInput);
                     });
