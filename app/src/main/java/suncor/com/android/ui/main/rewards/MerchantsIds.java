8d606ea457cfb5349e8f7ed27e2bc52b655916e5
Jaspreet Singh <jaspresingh@suncor.com>
2021-07-08 17:12:53 +0530

    -- Done with adding the petrocanada card in redeem

--- app/src/main/java/suncor/com/android/ui/main/rewards/MerchantsIds.java
+++ app/src/main/java/suncor/com/android/ui/main/rewards/MerchantsIds.java
@@ -5,10 +5,12 @@ public class MerchantsIds {
     public static final int Hudson_Bay_EN = 2288;
     public static final int Cineplex_EN = 3089;
     public static final int Cara_EN = 2306;
+    public static final int Petro_Canada_EN = 4677;
 
 
     public static final int WINNERS_HomeSense_Marshalls_FR = 3267;
     public static final int Hudson_Bay_FR = 2377;
     public static final int Cineplex_FR = 3090;
     public static final int Cara_FR = 2337;
+    public static final int Petro_Canada_FR = 4678;
 }
