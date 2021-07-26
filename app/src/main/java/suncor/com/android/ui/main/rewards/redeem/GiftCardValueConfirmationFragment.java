e1dd8419cd074481d5484cccbbf8c468ba21f471
Jaspreet Singh <jaspresingh@suncor.com>
2021-07-26 18:33:43 +0530

    Added the string for both the locales .

--- app/src/main/java/suncor/com/android/ui/main/rewards/redeem/GiftCardValueConfirmationFragment.java
+++ app/src/main/java/suncor/com/android/ui/main/rewards/redeem/GiftCardValueConfirmationFragment.java
@@ -213,6 +213,9 @@ public class GiftCardValueConfirmationFragment extends MainActivityFragment impl
     }
 
     private void cardValueChanged(Integer selectedItem) {
+        if(viewModel.getGiftCardItem().getTitle().equals(getString(R.string.merchant_petrocanada_card))){
+            return;
+        }
         int valueSelected = viewModel.getGiftCardItem().geteGifts().get(selectedItem).getPetroPointsRequired();
         int userPetroPoints = viewModel.getSessionManager().getProfile().getPointsBalance();
         viewModel.setEGift(viewModel.getGiftCardItem().geteGifts().get(selectedItem));
