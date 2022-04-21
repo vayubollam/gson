38163fe1945f7bef21fa550ca4cee8c58fc0b401
Jaspreet Singh <jaspresingh@suncor.com>
2022-04-21 14:33:39 +0530

    Changes made in date utils class.

--- app/src/main/java/suncor/com/android/utilities/Constants.kt
+++ app/src/main/java/suncor/com/android/utilities/Constants.kt
@@ -118,7 +118,8 @@ object Constants {
     const val GET_LOCATIONS_ADAPTER__PATH = "/adapters/suncor/v3/rfmp-secure/locations"
 
     //Image expiry time
-    const val IMAGE_EXPIRY_DATE = "2022/05/23 23:59:00"
+ //   const val IMAGE_EXPIRY_DATE = "2022/05/23 23:59:00"
+    const val IMAGE_EXPIRY_DATE = "2022/04/21 16:35:00"
 
     //Home page banners
     const val SIGN_IN_BANNER = "sign_in_banner"
