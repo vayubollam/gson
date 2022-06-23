9cd68ff960c3b078adc96af547ab16a7168bc54d
arranasuncor <97896195+arranasuncor@users.noreply.github.com>
2022-06-23 13:34:36 -0600

    Add response model, Define & implement api call method.

--- app/src/main/java/suncor/com/android/data/redeem/MerchantsApiImpl.java
+++ app/src/main/java/suncor/com/android/data/redeem/MerchantsApiImpl.java
@@ -19,6 +19,7 @@ import suncor.com.android.SuncorApplication;
 import suncor.com.android.mfp.ErrorCodes;
 import suncor.com.android.model.Resource;
 import suncor.com.android.model.merchants.Merchant;
+import suncor.com.android.model.redeem.response.MemberEligibilityResponse;
 import suncor.com.android.utilities.Timber;
 
 public class MerchantsApiImpl implements MerchantsApi {
@@ -69,6 +70,38 @@ public class MerchantsApiImpl implements MerchantsApi {
             result.postValue(Resource.error(ErrorCodes.GENERAL_ERROR));
         }
 
+        return result;
+    }
+
+    @Override
+    public LiveData<Resource<MemberEligibilityResponse>> getMemberEligibility() {
+        Timber.d("request to get member eligibility made");
+        MutableLiveData<Resource<MemberEligibilityResponse>> result = new MutableLiveData<>();
+        result.postValue(Resource.loading());
+        try {
+            URI adapterPath = new URI("/adapters/suncor/v1/rfmp-secure/get-member-eligibility");
+            WLResourceRequest request = new WLResourceRequest(adapterPath, WLResourceRequest.GET, SuncorApplication.PROTECTED_SCOPE);
+
+            request.send( new WLResponseListener() {
+                @Override
+                public void onSuccess(WLResponse wlResponse) {
+                    String jsonText = wlResponse.getResponseText();
+                    Timber.d("Member Eligibility api call success, response:\n" + jsonText);
+                    MemberEligibilityResponse response = gson.fromJson(jsonText, MemberEligibilityResponse.class);
+                    result.postValue(Resource.success(response));
+                }
+
+                @Override
+                public void onFailure(WLFailResponse wlFailResponse) {
+                    Timber.d("Member Eligibility api call failed, " + wlFailResponse.toString());
+                    Timber.e(wlFailResponse.toString());
+                    result.postValue(Resource.error(wlFailResponse.getErrorMsg()));
+                }
+            });
+        } catch (URISyntaxException e) {
+            Timber.e(e.toString());
+            result.postValue(Resource.error(ErrorCodes.GENERAL_ERROR));
+        }
         return result;
     }
 }
