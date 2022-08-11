5b0119ae644ec2dbef74d481057a28c2c8d17622
armaan <arrana@suncor.com>
2022-08-11 17:09:08 -0600

    made corrections to data when keyboard hides

--- app/src/main/java/suncor/com/android/ui/main/rewards/donate/DonatePetroPointsFragment.kt
+++ app/src/main/java/suncor/com/android/ui/main/rewards/donate/DonatePetroPointsFragment.kt
@@ -1,9 +1,14 @@
 package suncor.com.android.ui.main.rewards.donate
 
+import android.R
+import android.graphics.Rect
+import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.TypedValue
 import android.view.*
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.google.firebase.crashlytics.internal.common.CommonUtils.hideKeyboard
@@ -16,7 +21,7 @@ import java.util.*
 import javax.inject.Inject
 
 
-class DonatePetroPointsFragment : MainActivityFragment() {
+class DonatePetroPointsFragment : MainActivityFragment(), OnKeyboardVisibilityListener {
 
     private var isFrench: Boolean = false
 
@@ -77,33 +82,99 @@ class DonatePetroPointsFragment : MainActivityFragment() {
 
             override fun beforeTextChanged(
                 s: CharSequence, start: Int,
-                count: Int, after: Int) {}
+                count: Int, after: Int
+            ) {
+            }
 
             override fun onTextChanged(
                 s: CharSequence, start: Int,
-                before: Int, count: Int) {}
+                before: Int, count: Int
+            ) {
+            }
         })
 
         binding.inputField.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
-                hideKeyboard(context,v)
-                viewModel.rectifyValuesOnKeyboardGone()
-                binding.inputField.setText(viewModel.formattedDonationAmount.get())
-                binding.inputField.setSelection(viewModel.formattedDonationAmount.get()?.length ?: 0 )
-                    return@OnKeyListener true
+                hideKeyboard(context, v)
+                return@OnKeyListener true
             }
-           return@OnKeyListener false
+            return@OnKeyListener false
         })
-
-
 
         binding.vm = viewModel
 
         return binding.root
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            view.setWindowInsetsAnimationCallback(object : WindowInsetsAnimation.Callback(DISPATCH_MODE_STOP) {
+                override fun onProgress(
+                    insets: WindowInsets,
+                    runningAnimations: MutableList<WindowInsetsAnimation>
+                ): WindowInsets {
+                    return insets
+                }
+                override fun onEnd(animation: WindowInsetsAnimation) {
+                    super.onEnd(animation)
+                    val showingKeyboard = view.rootWindowInsets.isVisible(WindowInsets.Type.ime())
+                    onVisibilityChanged(showingKeyboard)
+                }
+            })
+        } else {
+            setKeyboardVisibilityListener(this);
+        }
+    }
+
+
+    override fun onVisibilityChanged(visible: Boolean) {
+        if(!visible){
+            viewModel.enableDonation.set(true)
+            viewModel.rectifyValuesOnKeyboardGone()
+            binding.inputField.setText(viewModel.formattedDonationAmount.get())
+            binding.inputField.setSelection(
+                viewModel.formattedDonationAmount.get()?.length ?: 0
+            )
+        }else{
+            viewModel.enableDonation.set(false)
+        }
+
+    }
+
+    private fun setKeyboardVisibilityListener(onKeyboardVisibilityListener: OnKeyboardVisibilityListener) {
+        val parentView = (binding.root.findViewById(R.id.content) as ViewGroup).getChildAt(0)
+        parentView.viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+            private var alreadyOpen = false
+            private val defaultKeyboardHeightDP = 100
+            private val EstimatedKeyboardDP =
+                defaultKeyboardHeightDP + if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) 48 else 0
+            private val rect: Rect = Rect()
+            override fun onGlobalLayout() {
+                val estimatedKeyboardHeight = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    EstimatedKeyboardDP.toFloat(),
+                    parentView.resources.displayMetrics
+                ).toInt()
+                parentView.getWindowVisibleDisplayFrame(rect)
+                val heightDiff: Int = parentView.rootView.height - (rect.bottom - rect.top)
+                val isShown = heightDiff >= estimatedKeyboardHeight
+                if (isShown == alreadyOpen) {
+                    Timber.i("Keyboard state", "Ignoring global layout change...")
+                    return
+                }
+                alreadyOpen = isShown
+                onKeyboardVisibilityListener.onVisibilityChanged(isShown)
+            }
+        })
+    }
     override fun onDestroy() {
         super.onDestroy()
         originalMode?.let { activity?.window?.setSoftInputMode(it) }
     }
+}
+
+interface OnKeyboardVisibilityListener {
+    fun onVisibilityChanged(visible: Boolean)
 }
