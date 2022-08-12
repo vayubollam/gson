064d16aa7980167a0c98d7e1438821b837922a05
Jaspreet Singh <jaspresingh@suncor.com>
2022-08-12 16:02:42 +0530

    Added one more image for donate functionality.

--- app/src/main/java/suncor/com/android/ui/main/rewards/donatepetropoints/ImageMapper.kt
+++ app/src/main/java/suncor/com/android/ui/main/rewards/donatepetropoints/ImageMapper.kt
@@ -13,42 +13,44 @@ object ImageMapper {
                     when(program.programId){
                         667.toLong() ->{
                             program.smallImage = R.drawable.caremakers_french_small
-                            program.smallImage = R.drawable.caremakers_french_large
+                            program.largeImage = R.drawable.caremakers_french_large
                         }
 
                         332.toLong() ->{
                             program.smallImage = R.drawable.olympic_card_small
-                            program.smallImage = R.drawable.olympic_card_large
+                            program.largeImage = R.drawable.olympic_card_large
                         }
 
                         312.toLong()->{
                             program.smallImage = R.drawable.paralympic_card_small
-                            program.smallImage = R.drawable.paralympic_card_large
+                            program.largeImage = R.drawable.paralympic_card_large
                         }
 
                         375.toLong()->{
-
+                            program.smallImage = R.drawable.coaching_association_small
+                            program.largeImage = R.drawable.coaching_association_large
                         }
                     }
                 }else{
                     when(program.programId){
                         667.toLong() ->{
                             program.smallImage = R.drawable.caremakers_small_english
-                            program.smallImage = R.drawable.caremakers_large_english
+                            program.largeImage = R.drawable.caremakers_large_english
                         }
 
                         332.toLong() ->{
                             program.smallImage = R.drawable.olympic_card_small
-                            program.smallImage = R.drawable.olympic_card_large
+                            program.largeImage = R.drawable.olympic_card_large
                         }
 
                         312.toLong()->{
                             program.smallImage = R.drawable.paralympic_card_small
-                            program.smallImage = R.drawable.paralympic_card_large
+                            program.largeImage = R.drawable.paralympic_card_large
                         }
 
                         375.toLong()->{
-
+                            program.smallImage = R.drawable.coaching_association_small
+                            program.largeImage = R.drawable.coaching_association_large
                         }
                     }
                 }
